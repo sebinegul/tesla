1412ba147b820e1e051a3d859e85f4921292f1ec
sebinegul <sebi.94george@gmail.com>
2021-10-01 12:47:35 +0530

    react reveal

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import carReducer from "../features/car/carSlice";
+const store = configureStore({
+  reducer: {
+    car: carReducer,
+  },
+});
+ export default store;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./app/store";
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
